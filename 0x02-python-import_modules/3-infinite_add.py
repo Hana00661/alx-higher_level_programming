64267b1d332cc933294da365dfe2f12fab166d1a
Hana00661 <eng.hana95@gmail.com>
2023-11-30 16:43:48 +0400

    modify

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,17 @@
 !/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
+import sys
 
-num1 = 0
-length = len(argv)
-for i in range(1, length):
-    num1 += int(argv[i])
-print('{}'.format(num1))
+number_of_arguments = len(sys.argv)
+
+if number_of_arguments == 1:
+    print(f"Number of argument(s): {number_of_arguments} argument:")
+else:
+    print(f"Number of argument(s): {number_of_arguments} arguments:")
+
+if number_of_arguments > 1:
+    print(".", end="")
+
+print("")
+
+for i in range(1, number_of_arguments):
+    print(f"{i}: {sys.argv[i]}")
