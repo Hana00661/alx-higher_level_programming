b6390aa568ef9a0dd777e31524f8e7d3176dec75
Hana00661 <eng.hana95@gmail.com>
2023-12-05 02:48:46 +0400

    me

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list == []:
-        return(None)
+        return (None)
     my_list.sort()
-    return(my_list[-1])
+    return (my_list[-1])
