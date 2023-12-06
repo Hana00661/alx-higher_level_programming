7291af49bd55781cc8a77cc175c0d14989f3b367
Hana00661 <eng.hana95@gmail.com>
2023-12-07 01:53:57 +0400

    22

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+    biggest = 0
+    biggest_key = ""
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+            biggest_key = key
+    return (biggest_key)
