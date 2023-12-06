a7e1291ef9f5ed3d08da6f32a440a687efd86079
Hana00661 <eng.hana95@gmail.com>
2023-12-07 02:06:23 +0400

    22

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
-        return 0
+        return (0)
     total = 0
     num = 0
     digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
@@ -9,4 +9,4 @@ def roman_to_int(roman_string):
     for i in reversed(roman_string):
         num = digits[i]
         total += num if total < num * 5 else -num
-    return total
+    return (total)
