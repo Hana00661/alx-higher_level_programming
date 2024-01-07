c0bd7fb82a8c223c540a8573cf1fd6ab39969c95
Hana00661 <eng.hana95@gmail.com>
2024-01-07 13:23:33 +0400

    sad

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a function add_integer that adds two integers.
+Attributes:
+    add_integer: function that adds two integers.
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integer and/or float values.
+
+    Args:a (int): First value
+        b (int, optional): Second value. Defaults to 98.
+    Returns: int: Sum of a and b.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,2 @@
+#what is TDD in python?
+_TDD (Test-Driven Development) is a software development approach that emphasizes writing tests before writing the actual code. It's a disciplined process that focuses on ensuring code quality and reliability through continuous testing and refinement.
