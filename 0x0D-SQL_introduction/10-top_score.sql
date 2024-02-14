fd8591041ceba52902709481d3ed7a2d2f496ca0
Hana00661 <eng.hana95@gmail.com>
2024-02-13 17:53:39 -0800

    add ..

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- script that lists all records of the table
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- script that displays the number of records
+SELECT COUNT(id)
+FROM first_table
+WHERE id = 89;
