ea5f427993c1ee6f144ff54c0514c1a2227ced56
Hana00661 <eng.hana95@gmail.com>
2024-03-13 03:36:21 +0400

    add

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const initDict = require('./101-data').dict;
+const newDict = {};
+
+for (const key in initDict) {
+  if (newDict[initDict[key]] === undefined) {
+    newDict[initDict[key]] = [];
+  }
+  newDict[initDict[key]].push(key);
+}
+console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
-
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const square = require('./5-square');
+class Square extends square {
+  /**
+   * @property {method} charPrint - prints the rectangle using the character c
+   * @returns void
+   */
   charPrint (c) {
     if (c === undefined) {
-      super.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
       }
+      console.log(s);
     }
   }
-};
+}
+module.exports = Square;
