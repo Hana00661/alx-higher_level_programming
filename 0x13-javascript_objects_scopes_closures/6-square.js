2286ff12fe6030013be10935661dd9aa5b0eed05
Hana00661 <eng.hana95@gmail.com>
2024-03-13 03:22:22 +0400

    the big error

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
