a3f7751ee6f42c4d3ebb62748dea76506ef45d14
Hana00661 <eng.hana95@gmail.com>
2024-03-31 10:40:48 +0400

    42

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+
 # sourcery skip: use-fstring-for-formatting
+
 """script that fetches https://intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
-    from urllib.request import urlopen
-
-    with urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+   
+    if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            print("Body response:")
+            print("\t- type:", type(content))
+            print("\t- content:", content)
+            print("\t- utf8 content:", content.decode('utf-8'))
+    except Exception as e:
+        print("Error:", e)
