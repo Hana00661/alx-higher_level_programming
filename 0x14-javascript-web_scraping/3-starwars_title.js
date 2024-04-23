b258189100540c4aa2cd0b3518a72500967de609
Hana00661 <eng.hana95@gmail.com>
2024-04-24 00:39:36 +0400

    git

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,7 @@
 const request = require('request');
 const baseUrl = 'https://swapi-api.hbtn.io/api/films';
 
+//  first argument is the movie ID
 const movieId = process.argv[2];
 
 request(`${baseUrl}/${movieId}/`, (error, response, body) => {
